552c9ff183bd680e24bee62d8cfbf5737ab79911
mathew pacheco <mathewgpacheco@gmail.com>
2021-10-09 12:02:35 -0400

    fixed params bug

--- routes/products.js
+++ routes/products.js
@@ -6,8 +6,8 @@ const orderMiddleware = require('../middleware/order');
 const reviewController = require('../controllers/reviews');
 const productMiddleware = require('../middleware/products');
 //user related routes
-router.post('/:productID/add', userMiddleware.authenticateToken, orderMiddleware.existCart, productController.add);
-router.post('/:productID/remove', userMiddleware.authenticateToken, productController.remove);
+router.post('/:productID/add', userMiddleware.authenticateToken, orderMiddleware.existCart, productMiddleware.product,productController.add);
+router.post('/:productID/remove', userMiddleware.authenticateToken,productMiddleware.product, productController.remove);
 router.get('/:productID/', userMiddleware.authenticateToken, productMiddleware.product, productController.getProduct);
 router.post('/product', userMiddleware.authenticateToken,productController.findProduct);
 router.post('/:productID/review', userMiddleware.authenticateToken, productMiddleware.product,reviewController.addReview);
