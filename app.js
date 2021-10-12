f1aaf0c596bacebe4109c6833adb1bfca60eea94
mathew pacheco <mathewgpacheco@gmail.com>
2021-10-12 01:49:06 -0400

    finished UI for landing page

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const indexRoutes = require('./routes/index');
 const userRoutes = require('./routes/users');
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/order');
+const path = require('path');
 const crawler = require('./crawler');
-
 
 mongoose.connect(
    'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@cluster0.xpgq3.mongodb.net/store?retryWrites=true&w=majority',
    {useUnifiedTopology:true},{autoIndex: false});
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
-
 const store = new MongoDBStore({
     uri:'mongodb+srv://' +process.env.DB_USER+ ':'+process.env.DB_PASS+'@cluster0.xpgq3.mongodb.net/sessions?retryWrites=true&w=majority',
     collection: 'sessiondata',
@@ -37,7 +37,7 @@ app.use(session({
 app.listen(port);
 console.log("Server listening at: " + port);
 
-crawler.initCrawler();
+//crawler.initCrawler();
 app.use(indexRoutes);
 app.use('/user/',userRoutes);
 app.use('/products/', productRoutes);
